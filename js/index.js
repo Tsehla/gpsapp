28b7a230639738e68504ffd18e2346f21809a831
Tsehla <petrusnk@gmail.com>
2018-01-19 11:59:43 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -18,54 +18,18 @@ function backgroundcolor(id, result){
 return document.getElementById(id).style.backgroundColor=result;
 
 }
+
+function divHideOrShow(id, hideOrShow){
+
+  return document.getElementById(id).style.display=hideOrShow;
+}
+
 //-------
 
-//geolocation finder
-
-if(navigator.geolocation){
-navigator.geolocation.watchPosition(myPosition, error, gpsOptions)
-}
-else{
-  alert("GPS signal not available, please enable gps and restart browser");
-  
-}
-
-function myPosition(position,latitudeStorage, longitudeStorage ){
-
- var longitude = position.coords.longitude;
- var latitude = position.coords.latitude;
-  
-  if(longitude!==longitudeStorage && latitude!==latitudeStorage){
-        //timer 
-    setTimeout(contentsChanger,3000);
-        
-    longitude!==longitudeStorage;
-    latitude!==latitudeStorage;
-   
-
-  }
-  
-  
-  return ;
-  
-}
-
-function error(){
-
-return alert("theres an error with gps connection, please check your phone settings or use different browser");
-
-}
-
-function gpsOptions(){
-
-enableHighAccuracy : true;
-
-}
-
-
-
-
+// content hide
+//document.getElementById("cartContent").style.display=none;
 //contents funxtion
+contentsChanger();
 
 
 function contentsChanger(){
@@ -97,31 +61,58 @@ var bodyArr=[[body1,body1a] ,[body2, body2a] ,[body3, body3a]];
 
 
 //header content
-document.getElementById("headerLogo").innerHTML='<img src="'+logoFinder+'">';
+document.getElementById("headerLogo").innerHTML='<img src="'+logoFinder+'" onerror="errorMessage()">';
 
 //body content
-document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'">';
+document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'" onerror="errorMessage()">';
+document.getElementById("imageMagnified").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'" onerror="errorMessage()">';
 
  //footer content
-document.getElementById("footerLeftContent").innerHTML='<img  src="'+bodyArr[logoArr.indexOf(logoFinder)][1]+'">';
-document.getElementById("footerRightContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'">';
+document.getElementById("footerLeftContent").innerHTML='<img  src="'+bodyArr[logoArr.indexOf(logoFinder)][1]+'" onerror="errorMessage()">';
+document.getElementById("footerRightContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'" onerror="errorMessage()">'; 
            
 //footer click capture
 document.getElementById("footerLeft").onclick=function(){
                    
-         document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][1]+'">';
+         document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][1]+'" onerror="errorMessage()">';
+         document.getElementById("imageMagnified").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][1]+'" onerror="errorMessage()">';
                               
            }
            
 document.getElementById("footerRight").onclick=function(){
                    
-         document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'">';
+         document.getElementById("bodyContent").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'" onerror="errorMessage()">';
+         document.getElementById("imageMagnified").innerHTML='<img src="'+bodyArr[logoArr.indexOf(logoFinder)][0]+'" onerror="errorMessage()">';
                               
            }    
            
     }
     
+//image load error catching
+function errorMessage(){
+        contentsChanger();
+       //location.reload(true);
+       alert("Issue loading content, retrying...");
+       
+       }
+    
+
+//timer 
+setInterval(contentsChanger,50000);
+
+
+//popups
+ function cartOpener(){
+document.getElementById("cartContent").style.display="block";
+ }
  
-
-
-
+//Magnify & cart closer
+ function closer(){
+ document.getElementById("cartContent").style.display="none";
+ document.getElementById("magnifyWindow").style.display="none";
+ }
+ function imageMagnify(){
+           document.getElementById("magnifyWindow").style.display="block";
+ }
+ 
+ 
